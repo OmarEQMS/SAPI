02d81d730460d784324b2e7849bbd2de03b17359
Omar Quintero <omar.quintero.ms@gmail.com>
2018-09-10 11:54:38 -0500

    Insertar con AJAX

---
+++ build/web/js/asincrono.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+   $("#btn-login").click(function(){
+      $.ajax({
+         url : "RegistraUsuarioController",
+         data :{
+             nombre : $("#nombre").val(),
+             apellidos : $("#apellidos").val(),
+             usuario : $("#usuario").val(),
+             password : $("#password").val(),
+             curp : $("#curp").val(),
+             telefono : $("#telefono").val(),
+             correo : $("#correo").val()
+         },
+         method : "POST",
+         error : function(xhr){
+             alert("Ops! Algo Paso");
+         },
+         success : function(response){
+             alert("El registro se inserto corretamente");
+             $("input").val("");
+         }
+      }); 
+   });    
+});
