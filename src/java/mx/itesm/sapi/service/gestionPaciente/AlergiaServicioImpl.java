3a3e7888a305bdd04f0a6e4f16ac5a087fd920de
Oscar Ernesto Miranda Palacios <a01422575@itesm.mx>
2018-10-11 18:23:04 -0500

    Correcion de Modelos OtroEstudio y Torre Nueva

--- src/java/mx/itesm/sapi/service/gestionPaciente/AlergiaServicioImpl.java
+++ src/java/mx/itesm/sapi/service/gestionPaciente/AlergiaServicioImpl.java
@@ -54,6 +54,37 @@ public class AlergiaServicioImpl implements AlergiaServicio{
 
     @Override
     public List<Alergia> mostrarAlergia() {
+        Connection conn;
+        CallableStatement cstmt;
+        ResultSet rs;
+        String stProcedure = "---";
+        List<Alergia> alergias = null;
+        Alergia alergia;
+
+	try{
+            conn  = Conexion.getConnection();
+            cstmt = conn.prepareCall(stProcedure);
+            rs = cstmt.executeQuery();
+            alergias =  new ArrayList<>();
+            
+            while(rs.next()){
+                alergia = new Alergia();
+                alergia.setIdAlergia(rs.getInt("idAlergia"));
+                alergia.setNombre(rs.getString("nombre"));
+
+                alergias.add(alergia);
+            }
+		
+		conn.close();
+		cstmt.close();
+		rs.close();
+                
+	}catch(SQLException ex){
+            System.out.println(this.getClass().toString().concat(Thread.currentThread().getStackTrace()[1].getMethodName())
+                    .concat(ex.getMessage()));
+            alergias = null;
+	}
+        return alergias;
     }
 
     @Override
