853f904e05c005318376181a96c2085f023c4b20
FerOrduna <A01422919@itesm.mx>
2018-10-11 18:39:53 -0500

    Registro junto con lo de neto v.2.

---
+++ src/java/mx/itesm/sapi/service/gestionPaciente/EstadoPacientePacienteService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itesm.sapi.service.gestionPaciente;
+
+import java.util.List;
+
+/**
+ *
+ * @author urieldiaz
+ */
+public interface EstadoPacientePacienteService {
+    public EstadoPacientePaciente mostrarEstadoPacientePaciente(int idEstadoPacientePaciente);
+    public List<EstadoPacientePaciente> mostrarEstadoPacientePaciente();
+    public int agregarEstadoPacientePaciente(EstadoPacientePaciente estadoPacientePaciente);
+    public boolean borradoLogicoEstadoPacientePaciente(int idEstadoPacientePaciente);
+    public boolean actualizarEstadoPacientePaciente(EstadoPacientePaciente EstadoPacientePaciente);
+}
