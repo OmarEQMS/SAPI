a06c2bfd0a44b766ca01e32d52f181ce5cd56c14
Alexis ER <alexiesr73c@gmail.com>
2018-10-29 08:30:10 -0600

    just in case

--- web/js/appLogin.js
+++ web/js/appLogin.js
@@ -106,7 +106,30 @@ $(document).ready(function () {
             key: "getRegistro"
         });
     });
-
+    $('#btn-login').on('click', function () {
+        var usu = $("#user");
+        var pass = $("#password");
+        $.get("LoginController", {
+            key: "verificar",
+            usuario: usu.val(),
+            password: pass.val()
+        },
+                function (response, status, xhr) {
+                    //console.log(response);
+                    if (status == "success") {
+                        if (response == "LoginError") { 
+                            //console.log("No se pudo inicar sesion");
+                            $("#msj-error").show();
+                        } else {
+                            $("#msj-cargando").show();
+                            document.open("text/html", "replace");
+                            document.write(response);
+                            document.close();
+                        }
+                    }
+                }
+        );
+    });
 });
 
 
